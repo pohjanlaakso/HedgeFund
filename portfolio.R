b703a34f9768e3ea454e23e06e7d4fe4d361656c
pohjanlaakso <pohjanlaakso@gmail.com>
2025-01-02 09:53:25 +0200

    Added a new feature (non-invetsment grade bonds)!

--- portfolio.R
+++ portfolio.R
@@ -2,3 +2,10 @@
 # Mise en place
 rm(list=ls()) # empty environment
 setwd(getwd()) # set working directory
+library(tidyquant) # getSymbols()
+
+# download data and visualize
+getSymbols('QQQ'); plot(QQQ$QQQ.Close) # Nasdaq 100
+getSymbols('VCLT'); plot(VCLT$VCLT.Close) # Long-term corporate bonds
+getSymbols('WIAU.L');plot(WIAU.L$WIAU.L.Close) # Non-investment grade bonds
+
