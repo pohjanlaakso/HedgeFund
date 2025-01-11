1f1bb84ea3bb4fe74c389001440193a45ad0b144
pohjanlaakso <pohjanlaakso@gmail.com>
2025-01-11 11:03:52 +0200

    Fetched commodities data and added return calculations.

--- risk_parity.R
+++ risk_parity.R
@@ -1,2 +1,12 @@
 
 source('portfolio.R')
+
+# fetch data, rename columns, and plot
+commodities <- getSymbols('GD=F', auto.assign = F)
+colnames(commodities) <- c('Open', 'High', 'Low', 'Close', 'Volume', 'Adjusted')
+plot(commodities$Close)
+
+# 
+r_commodities <- commodities$Close / lag(commodities$Close, k=1)-1
+r_commodities <- na.omit(r_commodities)
+plot(r_commodities)
